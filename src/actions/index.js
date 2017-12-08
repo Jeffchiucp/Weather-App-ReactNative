6a4dcb1980c26b988ce0ff4041a58768b7505f16
Jeff Chiu <jeffchiu2000@gmail.com>
2017-12-07 16:48:38 -0800

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 import axios from 'axios';
 
-const API_KEY = '9627493b7a3e420a74862d00e33ff91d';
+const API_KEY = process.env.API_KEY;
+
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
